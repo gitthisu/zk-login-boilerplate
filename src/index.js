e28a8bbc2bd1deb29ff4e01daf141ee39b0351ed
Pauline Laout <laout.pauline@hotmail.fr>
2024-02-13 22:01:51 +0100

    ready for testing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Callback from './Callback';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <h1>Sui ZK Login</h1>
+  <Router>
+    <Routes>
+      <Route path="/notes" element={<App />} />
+      <Route path="/" element={<Callback />} />
+    </Routes>
+  </Router>
 );
 
 reportWebVitals();
